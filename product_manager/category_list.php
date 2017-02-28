7e4387c8cb9f6dfea696fcc06e5ba3fa7bbfb0f9
BlazerR <br229@njit.edu>
2017-02-27 20:52:52 -0500

    20

--- product_manager/category_list.php
+++ product_manager/category_list.php
@@ -7,18 +7,12 @@
             <th>Name</th>
             <th>&nbsp;</th>
         </tr>        
-        
         -<!-- add category rows here -->
-        <?php foreach ($categories as $category_id): ?>
-        <tr>
-        <td><?php echo $category ['category_name']; ?></td>
-        <td>DELETE</td>
-        </tr>
-        <?php end foreach; ?>
     </table>
 
     <h2>Add Category</h2>
     <!-- add code for form here -->
+    
     <p><a href="index.php?action=list_products">List Products</a></p>
 
 </main>
