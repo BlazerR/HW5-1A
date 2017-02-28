bbb2d8d8ebbb269592fee4b765117adc39abf7b0
BlazerR <br229@njit.edu>
2017-02-28 18:19:49 -0500

    76

--- product_manager/category_list.php
+++ product_manager/category_list.php
@@ -1,4 +1,5 @@
 <?php include '../view/header.php'; ?>
+require_once('database.php');
 <main>
 
     <h1>Category List</h1>
@@ -7,17 +8,17 @@
             <th>Name</th>
             <th>&nbsp;</th>
         </tr>        
-        -<!-- add category rows here -->
+        <!-- add category rows here -->
     </table>
 
     <h2>Add Category</h2>
     <!-- add code for form here -->
-    
+
     <p><a href="index.php?action=list_products">List Products</a></p>
 
 </main>
-
 <?php include '../view/footer.php'; ?>
-<?php include ' product_list.php'; ?>
+
+
 
 
